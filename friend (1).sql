590d0deb6e7a75027a5dceef898c2703766d484e
Ariessa-QASANDRA <ariessa.qasandra@gmail.com>
2021-11-15 12:51:00 +0800

    lasst

--- friend (1).sql
+++ friend (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 15, 2021 at 05:30 AM
+-- Generation Time: Nov 15, 2021 at 05:49 AM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 7.3.28
 
